37187b1fb8ec07b58ef3b49b22e52e260a6cf788
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-05-03 01:42:38 +0600

    set gep api for load data form database

--- index.js
+++ index.js
@@ -10,16 +10,39 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nzb6w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("smartphoneWarehouse").collection("warehouse");
-  console.log("DB Connected so cool")
-  // perform actions on the collection object
-  client.close();
-});
+async function run(){
+  try{
+    await client.connect();
+    const productCollection = client.db('smartphoneWarehouse').collection('warehouse');
+    
+    //load data form database
+    app.get('/product',async(req,res)=>{
+      const query = {}; 
+      const cursor = productCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
 
+      //load single product
+      app.get('/product/:id', async(req, res) =>{
+        const id = req.params.id;
+        const query={_id: ObjectId(id)};
+        const product = await productCollection.findOne(query);
+        res.send(product);
+    });
+
+
+    })
+
+  }
+  finally{
+
+  }
+
+}
+run().catch(console.dir)
 
 
 
