6cf155e7db74c1d5ef8a099fd96bd9af121e7939
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-05-04 13:44:27 +0600

    create delit api for delit item

--- index.js
+++ index.js
@@ -1,66 +1,71 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const port=process.env.PORT || 5000;
-const app=express()
+const port = process.env.PORT || 5000;
+const app = express()
 
 //meddle ware
 app.use(cors());
-app.use(express.json()) 
+app.use(express.json())
 
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nzb6w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-async function run(){
-  try{
+async function run() {
+  try {
     await client.connect();
     const productCollection = client.db('smartphoneWarehouse').collection('warehouse');
-    
+
     //load data form database
-    app.get('/product',async(req,res)=>{
-      const query = {}; 
+    app.get('/product', async (req, res) => {
+      const query = {};
       const cursor = productCollection.find(query);
       const products = await cursor.toArray();
       res.send(products);
 
       //load single product
-      app.get('/product/:id', async(req, res) =>{
+      app.get('/product/:id', async (req, res) => {
         const id = req.params.id;
-        const query={_id: ObjectId(id)};
+        const query = { _id: ObjectId(id) };
         const product = await productCollection.findOne(query);
         res.send(product)
-    });
-    // update quantity of products
-    app.put("/product/:id", async (req, res) => {
-      const id = req.params.id;
-      const data = req.body;
-      console.log("from update api", data);
-      const filter = { _id: ObjectId(id) };
-      const options = { upsert: true };
+      });
+      // update quantity of products
+      app.put("/product/:id", async (req, res) => {
+        const id = req.params.id;
+        const data = req.body;
+        console.log("from update api", data);
+        const filter = { _id: ObjectId(id) };
+        const options = { upsert: true };
 
-      const updateDoc = {
+        const updateDoc = {
           $set: {
             quantity: data.quantity,
-              
-          },
-      };
 
-      const result = await productCollection.updateOne(
+          },
+        };
+
+        const result = await productCollection.updateOne(
           filter,
           updateDoc,
           options
-      );
-      
-      res.send(result);
-  });
+        );
 
-
+        res.send(result);
+      });
+      // Delete item
+      app.delete('/product/:id', async (req, res) => {
+        const id = req.params.id
+        const query = { _id: ObjectId(id) };
+        const result = await productCollection.deleteOne(query)
+        res.send(result)
+      })
     })
 
   }
-  finally{
+  finally {
 
   }
 
@@ -73,9 +78,9 @@ run().catch(console.dir)
 
 
 
-app.get('/',(req,res)=>{
-    res.send('running genuse server')
+app.get('/', (req, res) => {
+  res.send('running genuse server')
 })
-app.listen(port,()=>{
-    console.log('lising the port',port)
+app.listen(port, () => {
+  console.log('lising the port', port)
 })
